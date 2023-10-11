efc0cb46a260025c6cb9953bf10870f0c62dc217
1027bestlife <1027bestlife@gmail.com>
2023-10-11 21:06:20 +0900

    MyProfilecopy test commit

---
+++ src/MyProfilecopy.java
@@ -0,0 +1,7 @@
+public class MyProfilecopy {
+    public static void main(String[] args){
+        System.out.println("seonha");
+        System.out.println("1027bestlife@gmail.com");
+        System.out.println("여자");
+    }
+}
