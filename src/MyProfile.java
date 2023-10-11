252c1f083c60c43d82bd37e085a890c50e1b59a9
1027bestlife <1027bestlife@gmail.com>
2023-10-11 20:43:04 +0900

    first commit: MyProfile, Rectangle, Rectangle2, Rectangle3

---
+++ src/MyProfile.java
@@ -0,0 +1,7 @@
+public class MyProfile {
+    public static void main(String[] args){
+        System.out.println("seonha");
+        System.out.println("1027bestlife@gmail.com");
+        System.out.println("여자");
+    }
+}
