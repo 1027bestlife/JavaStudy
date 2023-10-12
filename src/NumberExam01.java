2b0ed74b8221b4fb18ede2a4702ceef5f5e99e9c
1027bestlife <1027bestlife@gmail.com>
2023-10-12 18:32:54 +0900

    NumberExam01, PrimitiveCasstingExam1

---
+++ src/NumberExam01.java
@@ -0,0 +1,14 @@
+public class NumberExam01 {
+    public static void main(String[] args) {
+        double d1 = 50;
+        double d2 = 500L;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        int i1 = (int)50.0;
+        int i2 = (int)25.6f;
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+}
---
+++ src/PrimitiveCastingExam1.java
@@ -0,0 +1,14 @@
+public class PrimitiveCastingExam1 {
+    public static void main(String[] args){
+        long x2 = 50;
+        int i2 = (int)x2;
+        System.out.println(x2);
+        System.out.println(i2);
+
+        //        long x3 =Long.MAX_VALUE;
+        //        int i3 = (int)x3;
+        //        System.out.println(x3);
+        //        System.out.println(i3);
+        //        오버플로우 현상
+    }
+}
